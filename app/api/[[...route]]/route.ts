e2391f132ff9b9bc3db810f97a7a72f6c47056bb
Leonardo Loureiro <Leonardo.Loureiro.2021@live.rhul.ac.uk>
2026-06-07 16:27:39 +0100

    Protected api. Added some api routes which cannot be accessed if user is not signed in.

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,15 +1,33 @@
 import { Hono } from 'hono';
 import { handle } from 'hono/vercel';
+import { z } from 'zod';
+import { zValidator } from '@hono/zod-validator';
+import { clerkMiddleware, getAuth } from '@hono/clerk-auth';
 
-export const runtmie = 'edge';
+
+export const runtime = 'edge';
 
 const app = new Hono().basePath('/api');
 
-app.get('/hello', (context) => {
-  return context.json({ 
-    message: 'Hello, World!' 
-  });
-});
+app
+  .get(
+    '/hello',
+    clerkMiddleware(),
+    (context) => {
+      const auth = getAuth(context);
 
+      if (!auth?.userId) {
+        return context.json({ message: 'Unauthorized' });
+      }
+
+      return context.json({ 
+        message: 'Hello, World!',
+        userID: auth.userId, 
+      });  
+    }
+  )
+
+// Instead of writing "const..." we simple pass it to handle and Hono handles it seamlessly.
+// This is a great way to keep our code clean and concise, especially when we have multiple routes to handle.
 export const GET = handle(app);
 export const POST = handle(app);
