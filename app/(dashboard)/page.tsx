6b281fc3666e5f1b589649ae977375f3d681af0c
Leonardo Loureiro <Leonardo.Loureiro.2021@live.rhul.ac.uk>
2026-06-07 15:30:56 +0100

    Implemented signing in w/ Clerk.

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,9 +1,8 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <p>
-      This is an authenticated route. You can only see this if you are signed in. If you are not signed in, you will be redirected to the sign-in page.
-    </p>
+    <UserButton />
   );
 }
