acd49f0b55847bd2c4a1a05936ace03c249cc131
Leonardo Loureiro <Leonardo.Loureiro.2021@live.rhul.ac.uk>
2026-06-09 11:37:25 +0100

    /api/accounts When accessing the route, Hono now runs db query and returns it to page, currently empty but works.

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,33 +1,18 @@
-import { Hono } from 'hono';
-import { handle } from 'hono/vercel';
-import { z } from 'zod';
-import { zValidator } from '@hono/zod-validator';
-import { clerkMiddleware, getAuth } from '@hono/clerk-auth';
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
+import accounts from "./accounts";
 
 
-export const runtime = 'edge';
+export const runtime = "edge";
 
-const app = new Hono().basePath('/api');
+const app = new Hono().basePath("/api");
 
-app
-  .get(
-    '/hello',
-    clerkMiddleware(),
-    (context) => {
-      const auth = getAuth(context);
-
-      if (!auth?.userId) {
-        return context.json({ message: 'Unauthorized' });
-      }
-
-      return context.json({ 
-        message: 'Hello, World!',
-        userID: auth.userId, 
-      });  
-    }
-  )
+const routes = app.
+  route("/accounts", accounts);
 
 // Instead of writing "const..." we simple pass it to handle and Hono handles it seamlessly.
 // This is a great way to keep our code clean and concise, especially when we have multiple routes to handle.
 export const GET = handle(app);
 export const POST = handle(app);
+
+export type AppType = typeof app;
