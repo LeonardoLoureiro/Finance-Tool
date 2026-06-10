5387e00b6f9d4a06617d4476dbe8e1eb9a152f4d
Leonardo Loureiro <Leonardo.Loureiro.2021@live.rhul.ac.uk>
2026-06-10 17:14:21 +0100

    Create account. Created new api used to create a new account in db.

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -1,8 +1,11 @@
-import { Hono } from "hono";
-import { db } from "@/db/drizzle"
-import { accounts } from "@/db/schema";
+import { db } from "@/db/drizzle";
+import { accounts, insertAccountsSchema } from "@/db/schema";
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
+import { zValidator } from "@hono/zod-validator";
+import { Hono } from "hono";
 import { HTTPException } from "hono/http-exception";
+import { createId } from "@paralleldrive/cuid2";
+import { convertSegmentPathToStaticExportFilename } from "next/dist/shared/lib/segment-cache/segment-value-encoding";
 
 const app = new Hono()
   .get("/" ,
@@ -27,6 +30,37 @@ const app = new Hono()
       }).from(accounts);
 
     return context.json({ data });
-});
+  })
+  .post(
+    "/",
+    clerkMiddleware(),
+    // just want to validate the name, since userId is set to notNull,
+    // but will use the userId to be the one from getAuth instead.
+    zValidator("json", insertAccountsSchema.pick({
+      name: true,
+    })),
+    async (context) => {
+      const auth = getAuth(context);
+      const values = context.req.valid("json");
+
+      if (!auth?.userId) {
+        throw new HTTPException(401, { 
+          res: context.json( { error: "Unauthorised."}, 401 )
+        });
+      }
+
+      // must chain 'returning' otherwise nothing will return 
+      // to object, which then can be used to return data below this.
+      const [data] = await db
+        .insert(accounts)
+        .values({
+          id: createId(),
+          userId: auth.userId,
+          ...values,
+      }).returning();
+
+      return context.json({ data });
+    }
+  );
 
 export default app;
