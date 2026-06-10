65f9f8a2bcc719712741afe52f93293e90438e85
Leonardo Loureiro <Leonardo.Loureiro.2021@live.rhul.ac.uk>
2026-06-10 16:32:58 +0100

    Deny access to /api/accounts if not logged in.

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -1,13 +1,22 @@
 import { Hono } from "hono";
 import { db } from "@/db/drizzle"
 import { accounts } from "@/db/schema";
+import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
 
 const app = new Hono()
-  .get("/" , async (context) => {
-    const data = await db.select({
-      id: accounts.id,
-      name: accounts.name,
-    }).from(accounts);
+  .get("/" ,
+    clerkMiddleware(), 
+    async (context) => {
+      const auth = getAuth(context);
+
+      if (!auth?.userId) {
+        return context.json({ "error": "Unauthorised." }, 401);
+      }
+
+      const data = await db.select({
+        id: accounts.id,
+        name: accounts.name,
+      }).from(accounts);
 
     return context.json({ data });
 });
