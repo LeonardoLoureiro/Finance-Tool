11942d69908a4bcb1edb88dd4697b44df38f98ca
Leonardo Loureiro <Leonardo.Loureiro.2021@live.rhul.ac.uk>
2026-06-10 16:33:18 +0100

    handle error. If something goes wrong fetching accounts.

--- features/accounts/api/user-get-accounts.ts
+++ features/accounts/api/user-get-accounts.ts
@@ -1,12 +1,25 @@
 import { useQuery } from "@tanstack/react-query";
 import { client } from "@/lib/hono";
 
+// hook
 export const useGetAccounts = () => {
   const query = useQuery({
     queryKey: ["accounts"],
     queryFn: async () => {
+      // fetch accounts from API, since using Hono client,
+      // if will simlply assign data to objects (ORM).
+      // Do not have to fetch and parse json, it is done by client
+      // and is end-to-end type safe.
       const res = await client.api.accounts.$get();
-      return res;
+
+      // if something goes wrong then throw an error.
+      if (!res.ok) {
+        throw new Error("Failed to fetch accounts");
+      }
+
+      const { data } = await res.json();
+
+      return data;
     }
   });
 
