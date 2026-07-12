39e84433e7e865ce019272a4242eed1392d88c79
Leonardo Loureiro <Leonardo.Loureiro.2021@live.rhul.ac.uk>
2026-07-12 15:06:44 +0100

    Changes to transactions columns. Now amount are colour coded depending if they are expenses or income.

--- app/(dashboard)/transactions/columns.tsx
+++ app/(dashboard)/transactions/columns.tsx
@@ -7,7 +7,7 @@ import { ColumnDef } from "@tanstack/react-table";
 import { InferResponseType } from "hono";
 import { ArrowUpDown } from "lucide-react";
 import { Actions } from "./actions";
-import { convertAmountFromMilliUnits } from "@/lib/utils";
+import { cn, convertAmountFromMilliUnits } from "@/lib/utils";
 
 export type ResponseType = InferResponseType<typeof client.api.transactions.$get, 200>["data"][0];
 
@@ -25,7 +25,7 @@ export const columns: ColumnDef<ResponseType>[] = [
           checked={table.getIsAllPageRowsSelected() || table.getIsSomePageRowsSelected()}
           onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
           aria-label="Select all"
-        />
+          />
       </div>
     ),
     cell: ({ row }) => (
@@ -34,32 +34,9 @@ export const columns: ColumnDef<ResponseType>[] = [
           checked={row.getIsSelected()}
           onCheckedChange={(value) => row.toggleSelected(!!value)}
           aria-label="Select row"
-        />
+          />
       </div>
     ),
-  },
-  {
-    accessorKey: "payee",
-    header: ({ column }) => (
-      <Button variant="ghost" onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>
-        Payee
-        <ArrowUpDown className="ml-2 h-4 w-4" />
-      </Button>
-    ),
-    filterFn: "includesString",
-  },
-  {
-    accessorKey: "amount",
-    header: ({ column }) => (
-      <Button variant="ghost" onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>
-        Amount
-        <ArrowUpDown className="ml-2 h-4 w-4" />
-      </Button>
-    ),
-    cell: ({ row }) => {
-      const amount = row.original.amount;
-      return <span className="font-mono">{convertAmountFromMilliUnits(amount)}</span>;
-    },
   },
   {
     accessorKey: "date",
@@ -72,12 +49,46 @@ export const columns: ColumnDef<ResponseType>[] = [
     cell: ({ row }) => <span>{new Date(row.original.date).toLocaleDateString()}</span>,
   },
   {
+    accessorKey: "amount",
+    header: ({ column }) => (
+      <Button variant="ghost" onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>
+        Amount
+        <ArrowUpDown className="ml-2 h-4 w-4" />
+      </Button>
+    ),
+    cell: ({ row }) => {
+      const amount = row.original.amount;
+      const isIncome = amount > 0;
+      const isExpense = amount < 0;
+      
+      return <span className={cn(
+        "font-mono",
+        isIncome && "text-emerald-500",
+        isExpense && "text-rose-500"
+      )}>
+        {convertAmountFromMilliUnits(amount)}
+      </span>;
+    },
+  },
+  {
+    accessorKey: "payee",
+    header: ({ column }) => (
+      <Button variant="ghost" onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>
+        Payee
+        <ArrowUpDown className="ml-2 h-4 w-4" />
+      </Button>
+    ),
+    filterFn: "includesString",
+  },
+  {
     accessorKey: "account",
     header: "Account",
+    cell: ({ row }) => <span>{row.original.account || "N/A"}</span>,
   },
   {
     accessorKey: "category",
     header: "Category",
+    cell: ({ row }) => <span>{row.original.category || "N/A"}</span>,
   },
   {
     id: "actions",
